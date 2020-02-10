9ff6e5009d54eb8b2df34c3e473f190d4d42dea5
theLavellan <60810756+theLavellan@users.noreply.github.com>
2020-02-10 15:24:05 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -106,11 +106,29 @@ client.on('message', msg => {
 });
 
 function randomMessage(){
-    var randomNumber = Math.round(Math.random()*2); // 0, 1 or 2
+    var randomNumber = Math.round(Math.random()*20); // 0, 1 or 2
     switch(randomNumber){
-        case 0: return 'Fact 1';
-        case 1: return 'Fact 2';
-        case 2: return 'Fact 3';
+        case 0: return 'The heart of a shrimp is located in its head.';
+        case 1: return 'A snail can sleep for three years.';
+        case 2: return 'Elephants are the only animal that cannot jump.';
+        case 3: return 'Kangaroos cannot fart.';
+        case 4: return 'Around 50 percent of orangutans have fractured bones, due to falling out of trees on a regular basis.';
+        case 5: return 'Otters are very cute, but have very dark and disturbing secrets.';
+        case 6: return 'Turtles can breathe through their butts.';
+        case 7: return 'Some tarantulas can make sounds';
+        case 8: return 'Japanese Macaques make snowballs for fun. They would probably like mudball fights. Probably.';
+        case 9: return 'A sheep, a duck and a rooster were the first passengers in a hot air balloon.';
+        case 10: return 'Ketchup was a medicine in the early 1800s.';
+        case 11: return 'Crows can hold grudges against specific individual people.';
+        case 12: return 'Penguings used to be about 2 meters (6 feet) tall.';
+        case 13: return 'Fact 3';
+        case 14: return 'Fact 3';
+        case 15: return 'Fact 3';
+        case 16: return 'Fact 3';
+        case 17: return 'Fact 3';
+        case 18: return 'Fact 3';
+        case 19: return 'Fact 3';
+        case 20: return 'Fact 3';
     }
 }
 
@@ -123,9 +141,9 @@ client.on('message', (message)=>{
 function randomMessage2(){
     var randomNumber = Math.round(Math.random()*2); // 0, 1 or 2
     switch(randomNumber){
-        case 0: return 'Joke 1';
-        case 1: return 'Joke 2';
-        case 2: return 'Joke 3';
+        case 0: return 'What is brown and sticky? A stick.';
+        case 1: return 'What do you call a fly without wins? A walk!';
+        case 2: return 'Do you know how long Ive been looking for you?!';
     }
 }
 
